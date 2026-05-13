7af73f873a39b7e28cccb2b732fb9fe6ba20fd1f
rahulgangadasu <rahulreddygangadasu2709@gmail.com>
2026-05-13 12:10:32 -0500

    Add signals

--- task-management-app/src/app/user/user.ts
+++ task-management-app/src/app/user/user.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
@@ -9,14 +9,15 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.css',
 })
 export class User {
-  selectedUser = DUMMY_USERS[randomIndex];
+  selectedUser = signal(DUMMY_USERS[randomIndex]);
+  imagePath = computed(() => '/assets/users/' + this.selectedUser().avatar);
 
-  get selectedUserImage() {
-    return '/assets/users/' + this.selectedUser.avatar;
-  }
+  // get selectedUserImage() {
+  //   return '/assets/users/' + this.selectedUser().avatar;
+  // }
 
   onUserClick() {
     const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser = DUMMY_USERS[randomIndex];
+    this.selectedUser.set(DUMMY_USERS[randomIndex]);
   }
 }
