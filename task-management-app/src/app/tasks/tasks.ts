b28f307b7366e1468e846d68cddfdbe72808b4fe
rahulgangadasu <rahulreddygangadasu2709@gmail.com>
2026-05-20 17:57:12 -0500

    Add new task component and integrate with tasks component

--- task-management-app/src/app/tasks/tasks.ts
+++ task-management-app/src/app/tasks/tasks.ts
@@ -1,16 +1,17 @@
 import { Component, Input } from '@angular/core';
 import { Task } from './task/task';
+import { NewTask } from './new-task/new-task';
 
 @Component({
   selector: 'app-tasks',
-  imports: [Task],
+  imports: [Task, NewTask],
   templateUrl: './tasks.html',
   styleUrl: './tasks.css',
 })
 export class Tasks {
   @Input({ required: true }) userId!: string;
   @Input({ required: true }) name!: string;
-
+  isAddingTask = false;
   dummyTasks = [
     {
       id: 't1',
@@ -41,5 +42,9 @@ export class Tasks {
 
   onCompleteTask(id: string) {
     this.dummyTasks = this.dummyTasks.filter((task) => task.id !== id);
+  }
+
+  onStartAddTask() {
+    this.isAddingTask = true;
   }
 }
